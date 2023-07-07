1c9f066b5b7ebbb39d914f9c7f5df82495d552ad
azil <854257920@qq.com>
2023-07-07 21:28:17 +0800

    commit

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: 'dist',
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue-demo' : '/'
+  publicPath: '/vue-demo/'
 })
